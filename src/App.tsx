aebdb4c75f1efb6b00a1a0069e2a0e86772a7763
Francisco Cumini Londero <cuminifrancisco@gmail.com>
2026-08-18 17:05:40 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-3xl font-bold underline'>
+        Hola Mundo
+      </div>
+    </>
+  )
+}
+
+export default App
